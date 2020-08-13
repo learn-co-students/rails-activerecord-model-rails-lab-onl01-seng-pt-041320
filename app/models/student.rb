6716e784081d16752065ee4a52aad96788900094
dnodolf1212 <dnodolf@gmail.com>
2020-08-13 12:29:26 -0700

    Done.

---
+++ app/models/student.rb
@@ -0,0 +1,6 @@
+class Student < ActiveRecord::Base 
+  def to_s 
+    @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
+    @student.first_name + " " + @student.last_name
+  end
+end
